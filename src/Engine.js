52293067f31a705aa19f36682db7f860c28ee80e
Quentin vauthier <Quentin vauthier>
2015-11-09 15:48:22 +0100

    Commit de Test Histoire 1

--- src/Engine.js
+++ src/Engine.js
@@ -1,8 +1,18 @@
-'use strict';
+var Engine = function(){
+    var nbBilles = 0;
 
-var Engine = function () {
+    var t = Array(6); //Tableau de taille 6
+    for(var i=0;i<6;i++) {
+        t[i] = new Array(6); //Ajoute dans chaque tableau 6 ligne
+    this.ajouterBilles = function(position,couleur) {
+        colonne = position.charCodeAt(0)-97;
+        ligne = position.charCodeAt(1)-49;
+        t[colonne][ligne] = couleur;
+        nbBilles++;
+    }
 
-// private attributes and methods
+}
+}
 
-// public methods
-};
+
+
--- test/test.js
+++ test/test.js
@@ -3,4 +3,10 @@
 var PalettoTestCase = TestCase("PalettoTestCase");
 
 PalettoTestCase.prototype.testStory1 = function () {
+    var plateau = new Engine();
+    plateau.ajouterBilles("a1")
+    //assertTrue(plateau.getPlaying() == "noir")
+
+
+
 };
