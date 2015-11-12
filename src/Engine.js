6682baf393e82131c0720099c7f8538d1403a741
Quentin vauthier <Quentin vauthier>
2015-11-12 10:08:26 +0100

    Fin de l'histoire 1

--- src/Engine.js
+++ src/Engine.js
@@ -4,14 +4,45 @@ var Engine = function(){
     var t = Array(6); //Tableau de taille 6
     for(var i=0;i<6;i++) {
         t[i] = new Array(6); //Ajoute dans chaque tableau 6 ligne
+    }
     this.ajouterBilles = function(position,couleur) {
         colonne = position.charCodeAt(0)-97;
         ligne = position.charCodeAt(1)-49;
         t[colonne][ligne] = couleur;
+        //console.log(t[colonne][ligne])
         nbBilles++;
     }
 
-}
+        this.getNbBilles = function() {
+            return nbBilles;
+
+        }
+
+
+    this.juxtaposition = function() {
+        //lecture colonne par colonne
+        for(var i=0;i<6;i++) {
+            for(var j=0;j<6;j++) {
+                if(i!=0 && t[i][j] == t[i-1][j]) {
+                    return false;
+                }
+                if(i!= t.length-1 && t[i][j] == t[i+1][j]) {
+                    return false;
+                }
+                if(j!= 0 && t[i][j] == t[i][j-1]) {
+                    return false;
+                }
+                if(j!= t.length-1 && t[i][j] == t[i][j+1]) {
+                    return false;
+                }
+            }
+        }
+        return true
+    }
+
+
+
+
 }
 
 
--- test/test.js
+++ test/test.js
@@ -4,8 +4,50 @@ var PalettoTestCase = TestCase("PalettoTestCase");
 
 PalettoTestCase.prototype.testStory1 = function () {
     var plateau = new Engine();
-    plateau.ajouterBilles("a1")
-    //assertTrue(plateau.getPlaying() == "noir")
+    //36 BIlles
+    plateau.ajouterBilles("a1","noir")
+    plateau.ajouterBilles("a2","jaune")
+    plateau.ajouterBilles("a3","bleu")
+    plateau.ajouterBilles("a4","rouge")
+    plateau.ajouterBilles("a5","blanc")
+    plateau.ajouterBilles("a6","jaune")
+
+    plateau.ajouterBilles("b1","vert")
+    plateau.ajouterBilles("b2","blanc")
+    plateau.ajouterBilles("b3","jaune")
+    plateau.ajouterBilles("b4","noir")
+    plateau.ajouterBilles("b5","vert")
+    plateau.ajouterBilles("b6","bleu")
+
+    plateau.ajouterBilles("c1","blanc")
+    plateau.ajouterBilles("c2","vert")
+    plateau.ajouterBilles("c3","bleu")
+    plateau.ajouterBilles("c4","rouge")
+    plateau.ajouterBilles("c5","jaune")
+    plateau.ajouterBilles("c6","noir")
+
+    plateau.ajouterBilles("d1","bleu")
+    plateau.ajouterBilles("d2","rouge")
+    plateau.ajouterBilles("d3","blanc")
+    plateau.ajouterBilles("d4","vert")
+    plateau.ajouterBilles("d5","noir")
+    plateau.ajouterBilles("d6","rouge")
+
+    plateau.ajouterBilles("e1","rouge")
+    plateau.ajouterBilles("e2","jaune")
+    plateau.ajouterBilles("e3","noir")
+    plateau.ajouterBilles("e4","bleu")
+    plateau.ajouterBilles("e5","jaune")
+    plateau.ajouterBilles("e6","vert")
+
+    plateau.ajouterBilles("f1","blanc")
+    plateau.ajouterBilles("f2","bleu")
+    plateau.ajouterBilles("f3","rouge")
+    plateau.ajouterBilles("f4","blanc")
+    plateau.ajouterBilles("f5","vert")
+    plateau.ajouterBilles("f6","noir")
+    plateau.juxtaposition()
+    assertTrue(plateau.juxtaposition() == true)
 
 
 
